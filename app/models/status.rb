1a8650e47df2ad7a119e31d4d04181162c0cbda3
alaughlin <adamlaughlin@outlook.com>
2014-07-28 17:52:10 -0400

    Will query database for saved posts if offline. Else, first gets new posts.

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,3 +1,5 @@
+require 'open-uri'
+
 class Status < ActiveRecord::Base
   validates :twitter_user_id, :twitter_status_id, :body, presence: true
 
@@ -9,6 +11,8 @@ class Status < ActiveRecord::Base
     status = self.parse_json(JSON.parse(tweet))
 
     self.save_records([status], status.twitter_user_id)
+
+    nil
   end
 
   def self.fetch_by_twitter_user_id!(twitter_user_id)
@@ -20,7 +24,13 @@ class Status < ActiveRecord::Base
        status_arr << self.parse_json(tweet)
     end
 
+    status_arr.each do |status|
+      puts "#{status.body}"
+    end
+
     self.save_records(status_arr, twitter_user_id)
+
+    nil
   end
 
   def self.parse_json(tweet)
@@ -40,6 +50,28 @@ class Status < ActiveRecord::Base
 
     statuses.each do |status|
       status.save!
+    end
+
+    nil
+  end
+
+  def self.get_by_twitter_user_id(twitter_user_id)
+    if self.internet_connection?
+      self.fetch_by_twitter_user_id!(twitter_user_id)
+    else
+      self.where(twitter_user_id: twitter_user_id).each do |status|
+        puts "#{status.body}"
+      end
+    end
+
+    nil
+  end
+
+  def self.internet_connection?
+    begin
+      true if open("http://www.google.com/")
+    rescue
+      false
     end
   end
 end
